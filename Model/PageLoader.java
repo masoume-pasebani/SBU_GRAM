d81b42bdcc90c3eda1c6c637fe5acead5655ee8b
masoume pasebani <mwsmee@gmail.com>
2021-06-03 19:56:10 +0430

    pageloader updated.

--- Model/PageLoader.java
+++ Model/PageLoader.java
@@ -4,7 +4,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+
 
 import java.io.IOException;
 import java.nio.file.Paths;
