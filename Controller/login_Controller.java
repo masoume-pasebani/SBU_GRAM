18651f0eacf11d73533f27f2e524e9e8f185b321
masoume pasebani <mwsmee@gmail.com>
2021-06-07 12:41:50 +0430

    login changed.

--- Controller/login_Controller.java
+++ Controller/login_Controller.java
@@ -1,12 +1,11 @@
 package Controller;
 
 
-import Help.Validation;
+import Common.Help.Validation;
 import Model.PageLoader;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
