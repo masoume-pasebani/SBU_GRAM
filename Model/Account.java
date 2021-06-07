0bd0f022fb6856976b9129dec60e4762bddca779
masoume pasebani <mwsmee@gmail.com>
2021-06-07 14:05:04 +0430

    account updated.

--- Model/Account.java
+++ Model/Account.java
@@ -2,28 +2,44 @@ package Model;
 
 import Common.Help.Password;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class Account implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 161207394374102042L;
     private int followers = 0;
     private int following = 0;
 
-    private final String username;
+    private String username;
     private String password;
     private String name;
     private String lastname;
     private String phonenumber;
     private String birth;
 
-//    private ClientOutputHandler clientOutputHandler;
-//    private ClientInputHandler clientInputHandler;
+    private ArrayList<Account> followers_list=new ArrayList<>();
+    private ArrayList<Account> following_list=new ArrayList<>();
     private ArrayList<Account> blockedAccounts = new ArrayList<>();
 
-    public Account(String username, String password) {
+    public Account(String username, String password,String name,String lastname,String phonenumber,String birth) {
         this.username = username;
         this.password = password;
+        this.name=name;
+        this.lastname=lastname;
+        this.phonenumber=phonenumber;
+        this.birth=birth;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getName() {
@@ -38,16 +54,12 @@ public class Account implements Serializable {
         return lastname;
     }
 
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
+    public String getPassword() {
+        return password;
     }
 
     public String getPhonenumber() {
         return phonenumber;
-    }
-
-    public void setPhonenumber(String phonenumber) {
-        this.phonenumber = phonenumber;
     }
 
     public String getBirth() {
@@ -58,34 +70,33 @@ public class Account implements Serializable {
         this.birth = birth;
     }
 
-    public boolean checkPassword(String password) {
-        return this.password.equals(Password.generateHash(password));
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
     }
 
-//    public void changePassword(String currentPassword, String newPassword) {
-//        if (!checkPassword(currentPassword))
-//            throw new PasswordsDoNotMatchException();
-//
-//        this.password = Password.generateHash(newPassword);
-//    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-    public void changeName(String name){
-        this.name=name;
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
     }
-    public void changeLastname(String lastname){
-        this.lastname=lastname;
+
+    public boolean checkPassword(String passwordHash) {
+        return this.password.equals(Password.generateHash(passwordHash));
     }
-    public void changePhoneNumber(String phonenumber){
-        this.phonenumber=phonenumber;
-    }
-    public void changeBirthYear(String birth){
-        this.birth=birth;
-    }
-    public int incrementFollwers(){
+
+    public int incrementFollowers(){
         return followers++;
     }
     public int incrementFollowing(){
         return following++;
+    }
+    public int decrementFollowers(){
+        return followers--;
+    }
+    public int decrementFollowing(){
+        return following--;
     }
 
     public int getFollowers() {
@@ -96,31 +107,18 @@ public class Account implements Serializable {
         return following;
     }
 
-//    public ClientOutputHandler getClientOutputHandler() {
-//        return clientOutputHandler;
-//    }
-//
-//    public void setClientOutputHandler(ClientOutputHandler clientOutputHandler) {
-//        this.clientOutputHandler = clientOutputHandler;
-//    }
-//
-//    public ClientInputHandler getClientInputHandler() {
-//        return clientInputHandler;
-//    }
-//
-//    public void setClientInputHandler(ClientInputHandler clientInputHandler) {
-//        this.clientInputHandler = clientInputHandler;
-//    }
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Account)) {
+            return false;
+        }
+        return ((Account) o).getUsername().equals(this.getUsername());
+    }
 
-//    public void userLoggedOut(){
-//        clientInputHandler = null;
-//        clientOutputHandler = null;
-//        currentOpponentOrRequestedAccount = null;
-//    }
-    //    public class PasswordNotLongEnoughException extends Exception {
-//    }
-//    public class PasswordShouldntContainNonDigitsOrLatinLettersException extends Throwable {
-//    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
 
     public void blockAccount(Account account){
         Objects.requireNonNull(account);
